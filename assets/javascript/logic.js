83455a47baa3140c58cd9b1154e605f7769d6c1e
Rhonda Kremer <rhondakremer@gmail.com>
2019-07-23 23:03:06 -0400

    tried lots of things but changing src on click still does not work

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -17,16 +17,21 @@ function renderButtons() {
 
 
 $("#submit").on("click", function () {
+    if ($("#userInput").val() == "") {
+        alert("please enter a value");
+    } else {
     event.preventDefault();
-    $("#buttons-go-here").empty();
     var userInput = $("#userInput").val().trim();
     animals.push(userInput);
     $("#userInput").val("");
+    $("#buttons-go-here").empty();
     renderButtons();
+   // return;
+}
 });
 
 
-$(".animals").on("click", function () {
+$("#buttons-go-here").on("click", ".animals", function () {
     console.log("I should work")
     var animal = $(this).attr("data-animal");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -39,26 +44,59 @@ $(".animals").on("click", function () {
         console.log("this works too");
         console.log(response);
         var results = response.data;
+       //$("#buttons-go-here").empty();
        
-        for (var i = 0; i < animals.length; i++) {
-            var animalSpan = $("<span class='gifs'>");
+        for (var i = 0; i < 10; i++) {
+            var animalDiv = $("<div class='gifs'>");
             var newGIF = $("<p>").text(results[i].rating);
-       
             var animalImage = $("<img>");
-            animalImage.attr("src", results[i].images.fixed_height.url);
+
+            animalImage.attr("src", results[i].images.fixed_height_still.url);
+            animalImage.attr("data-state", "still");
+            animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+            animalImage.attr("data-animate", results[i].images.fixed_height.url);
+
+
+            animalDiv.append("Rating: ");
+            animalDiv.append(newGIF);
+        
+            animalDiv.append(animalImage);
+        
+            $("#gifs-go-here").prepend(animalDiv);
             
-            animalSpan.append("Rating: ");
-            animalSpan.append(newGIF);
-        
-            animalSpan.append(animalImage);
-        
-            $("#gifs-go-here").prepend(animalSpan);
      }
 
     });
 });
 
 
+
+function changeState() {
+    
+    var state = $(this).attr("data-state");
+    console.log(this);
+    var animate = $(this).attr("data-animate");
+    var still = $(this).attr("data-still");
+
+
+    if (state == "still") {
+        console.log("I am still")
+        $(this).attr("src", animate);
+        $(this).attr("data-state", "animate");
+        return;
+    } else {
+        console.log("I am animated")
+        $(this).attr("src", still);
+        $(this).attr("data-state", "still");
+        return;
+    }
+
+};
+
+$("#gifs-go-here").on("click", ".gifs", function() {
+    console.log("I'm changing state");
+    changeState();
+});
 
 
 });
